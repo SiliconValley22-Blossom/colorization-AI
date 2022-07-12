ed6d79b1e0ac704d68c40c965e738a55e6cd7ba0
Hyeonjun Park <phjppo0918@gmail.com>
2022-07-12 18:56:46 +0900

    Auto stash before rebase of "refs/heads/main"

--- app.py
+++ app.py
@@ -1,11 +1,23 @@
-from flask import Flask
+from flask import Flask, send_file
+from flask import request
+from service import generate
+from util import preprocess_img, load_img, postprocess_tens, serve_pil_image
+import matplotlib.pyplot as plt
+from PIL import Image
 
 app = Flask(__name__)
 
+gen = generate(pretrained=True).eval()
 
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+
+@app.route('/image', methods=['post'])
+def post_image():  # put application's code here
+    file = request.files['file']
+    img, fm = load_img(file)
+    (tens_l_orig, tens_l_rs) = preprocess_img(img, HW=(256, 256))
+    result = postprocess_tens(tens_l_orig, gen(tens_l_rs).cpu())
+
+    return serve_pil_image(result, fm)
 
 
 if __name__ == '__main__':
---
+++ util/__init__.py
@@ -0,0 +1,1 @@
+from .imageLoader import *
